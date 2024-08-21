a79486427a851875fc0a4d06d17365be985e7198
chandresh <chandresh@depasserinfotech.com>
2024-08-21 11:46:34 +0530

    [via melty] feat: Add Breadcrumb and Input components

---
+++ src/components/Breadcrumb.tsx
@@ -0,0 +1,27 @@
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb"
+import { Input } from "@/components/ui/input"
+ 
+export function BreadcrumbResponsive() { 
+  return (
+    <Breadcrumb>
+      <BreadcrumbList>
+        <BreadcrumbItem>
+          <BreadcrumbLink>Home</BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbSeparator>/</BreadcrumbSeparator>
+        <BreadcrumbItem>
+          <BreadcrumbPage>
+          <Input type="text" name="dashboardName" id="dashboardName" value="My Dashboard" className="border-transparent hover:border hover:border-black" />
+          </BreadcrumbPage>
+        </BreadcrumbItem>
+      </BreadcrumbList>
+    </Breadcrumb>
+  )
+}
